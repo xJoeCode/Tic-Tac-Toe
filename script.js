4710be4191a1f9628ce7d2f823352f37042490f6
xJoeCode <xjosephtbr@gmail.com>
2022-06-16 21:16:34 +0800

    changed game logic

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
+let turn = 0;
+
 const player = (name, color) => {
-    let gameBoard = []
-    let turn = 0;
     const getName = () => name
     const getColor = () => color
-
     const start = () => {
-        turn++
+        turn += 1
+        console.log("start")
     }
 
     
-    const play = () => {
+    const play = (player2) => {
+        turn++
+        let controller = new AbortController()
         const gamePanel = document.querySelectorAll(".gamePanel")
         gamePanel.forEach(gamePanel => gamePanel.addEventListener('click', function playermark(e){
-            if (turn > 0){
-            console.log(getColor)
-            e.target.style.backgroundColor = color
-            const datakey = e.target.getAttribute("data-key")
-            gameBoard.push(datakey)
-            console.log(gameBoard)
-            turn--
-
+            console.log(turn)
+            if (turn == 1){
+                console.log(getColor)
+                e.target.style.backgroundColor = color
+                const datakey = e.target.getAttribute("data-key")
+                gameBoard.push(datakey)
+                console.log(gameBoard)
+                turn--
+                controller.abort()
+                
                 }
-            }))
+            },{signal:controller.signal}))
     }
 
-    const passturn = (name) =>{
-        if (turn > 0){
-            console.log("passing turn")
-            turn--
-            name.turn++
-            name.play("red")
-        }
-    }
 
-    return{start, getName,getColor,play,passturn}
+    return{start,getName,getColor,play,turn}
 
 }
 
 
 
-
-
-const tom = player("tom", "blue")
-const jimmy = player("jimmy", "red")
-tom.start()
-tom.play()
-jimmy.play()
-
-
-
-const gameOn = () =>{
-    
-    for (let i= 0; i<10; i++ ){
-        player1.play(1,red)
-        player2.play(1,blue)
-    }
+const gameOn = (name1, name2) => {
+    let gameBoard = []
+    const player1 = player(`${name1}`, "blue")
+    const player2 = player(`${name2}`, "red")
+    player1.turn += 1
+    console.log(player1)
+    console.log(player2)
+    const gamePanel = document.querySelectorAll(".gamePanel")
+    gamePanel.forEach(gamePanel => gamePanel.addEventListener('click', function playermark(e){
+        if(player1.turn > 0){
+            console.log(player1.getColor)
+            e.target.style.backgroundColor = player1.getColor()
+            const datakey = e.target.getAttribute("data-key")
+            gameBoard.push( `player1 key =${datakey}`)
+            console.log(gameBoard)
+            player1.turn--
+            player2.turn++
+            } else if (player2.turn > 0){
+                e.target.style.backgroundColor = player2.getColor()
+                const datakey = e.target.getAttribute("data-key")
+                gameBoard.push(`player2 key =${datakey}`)
+                console.log(gameBoard)
+                player2.turn--
+                player1.turn++
+            }
+    }))
 }
+
+gameOn("tom", "jimmy")
