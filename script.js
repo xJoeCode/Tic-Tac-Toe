c2627f21774ef2dcc0c0f09046a1a31ede93bf8d
xJoeCode <xjosephtbr@gmail.com>
2022-06-17 19:24:50 +0800

    changed the checkwinCondition function to use variables

--- script.js
+++ script.js
@@ -13,28 +13,31 @@ const gameOn = (name1, name2) => {
     let gameBoard = []
     const player1 = player(`${name1}`, "blue")
     const player2 = player(`${name2}`, "red")
+    console.log(typeof player1.getName())
     player1.turn++
     const gamePanel = document.querySelectorAll(".gamePanel")
     gamePanel.forEach(gamePanel => gamePanel.addEventListener('click', function playermark(e){
+
         if(player1.turn > 0 && e.target.style.backgroundColor != player1.getColor() && e.target.style.backgroundColor != player2.getColor()){
-            console.log(player1.getColor)
             e.target.style.backgroundColor = player1.getColor()
             const datakey = e.target.getAttribute("data-key")
             gameBoard.push(`player1${datakey}`)
             //console.log(gameBoard)
-            checkWinCondition()
+            checkWinCondition("player1")
             player1.turn--
             player2.turn++
+
             } else if (player2.turn > 0 && e.target.style.backgroundColor != player1.getColor() && e.target.style.backgroundColor != player2.getColor()){
                 e.target.style.backgroundColor = player2.getColor()
                 const datakey = e.target.getAttribute("data-key")
-                gameBoard.push(`player2 ${datakey}`)
+                gameBoard.push(`player2${datakey}`)
                 //console.log(gameBoard)
+                checkWinCondition("player2")
                 player2.turn--
                 player1.turn++
             }
     }))
-    function checkWinCondition(){
+    function checkWinCondition(_player){
 
         wincondition = {
             winCondition1: ['01', '02' ,'03'],
@@ -43,24 +46,28 @@ const gameOn = (name1, name2) => {
             winCondition3: ['02', '05', '08'],
             winCondition4: ['03', '05', '07'],
             winCondition5: ['03', '06', '09'],
-            winCondition6: ['04','05','06'],
+            winCondition6: ['04', '05', '06'],
             winCondition7: ['07', '08', '09']
         }
         
-        // const winCondition2 = 
-        const player1 = (gameBoard.filter(player => player.includes("player1")).map( n => n.replace('player1','')))
+        // removes the player name from the array
+        const playerArray = (gameBoard.filter(player => player.includes(`${_player}`)).map(n => n.replace(`${_player}`,'')))
+        console.log(playerArray)
+        
+        // loops every array in the wincondition object to check who matches the win conditions
         for (array in wincondition){
-           const player1Win = checkWin(wincondition[array])
-           if (player1Win){
-               console.log(player1Win)
-               alert("Player 1 Wins")
+           const playerWin = checkWin(wincondition[array])
+           console.log(playerWin)
+           if (playerWin){
+               console.log(playerWin)
+               alert(`${_player} Wins`)
             }
-            
         }
-        function checkWin(winCondition1){
-           return winCondition1.every(n => player1.includes(n))
+        // checks player array to see if the array matches the win conditions
+        function checkWin(winCondition){
+           return winCondition.every(n => playerArray.includes(n))
         } 
-        console.log(player1)
+        console.log(_player)
                
     }
 }
